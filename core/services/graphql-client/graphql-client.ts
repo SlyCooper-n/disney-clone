471ee6a078163440b2f9e86f6d375db93ce5456c
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-03 20:45:46 -0300

    feat(graphql-client): add authorization token

--- core/services/graphql-client/graphql-client.ts
+++ core/services/graphql-client/graphql-client.ts
@@ -4,6 +4,9 @@ import { GraphQLClientService } from "@core/types";
 export const client = new ApolloClient({
   uri: "https://api-us-east-1.hygraph.com/v2/cl7csc6zm4gdr01t24df50scw/master",
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: process.env.NEXT_PUBLIC_HYGRAPH_TOKEN as string,
+  },
 });
 
 export class GraphQLClient implements GraphQLClientService {
