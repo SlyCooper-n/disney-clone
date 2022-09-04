ca0fe03dd57248341e12d48f362654b8b37dcf52
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-04 03:19:29 -0300

    feat(types): add type for homepage query response

--- core/types/types.ts
+++ core/types/types.ts
@@ -1,3 +1,22 @@
+// * queries response
+export type HomepageQuery = {
+  page: {
+    homepage: {
+      slider: null;
+      brands: {
+        id: string;
+        slug: string;
+        brandLogo: {
+          url: string;
+        };
+        backgroundGif: {
+          url: string;
+        };
+      }[];
+    };
+  };
+};
+
 // * contexts
 export type ThemeContextValue = {
   appTheme: "light" | "dark";
