c578977968c0e4c172e16a2b06c335e9d55a4c8e
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-05 02:50:59 -0300

    feat(graphql-queries, pages/): add new queries and populate pages with respective data

--- core/types/types.ts
+++ core/types/types.ts
@@ -1,4 +1,14 @@
 // * queries response
+export type BrandPageQuery = {
+  brand: {
+    backgroundVideo: {
+      url: string;
+    };
+    backgroundImage: {
+      url: string;
+    };
+  };
+};
 export type HomepageQuery = {
   page: {
     homepage: {
@@ -16,16 +26,31 @@ export type HomepageQuery = {
     };
   };
 };
+export type OriginalsVideosQuery = VideosByTypeQuery;
 
-export type BrandPageQuery = {
-  brand: {
-    backgroundVideo: {
-      url: string;
-    };
-    backgroundImage: {
+export type VideoByIDQuery = {
+  video: {
+    mp4: {
       url: string;
     };
   };
+};
+
+export type VideoBySlugQuery = {
+  videos: {
+    videoInfo: {
+      title: string;
+      description: string;
+      releaseYear: number;
+      genre: string[];
+      banner: {
+        url: string;
+      };
+      videoLogo: {
+        url: string;
+      };
+    };
+  }[];
 };
 
 export type VideosByTypeQuery = {
@@ -45,8 +70,6 @@ export type VideosByTypeQuery = {
     };
   }[];
 };
-
-export type OriginalsVideosQuery = VideosByTypeQuery;
 
 // * contexts
 export type ThemeContextValue = {
--- pages/video/[videoID]/index.tsx
+++ pages/video/[videoID]/index.tsx
@@ -1,5 +1,51 @@
-const Video = () => {
+import { gql } from "@apollo/client";
+import { VIDEO_BY_ID } from "@core/graphql";
+import { client } from "@core/services";
+import { VideoByIDQuery } from "@core/types";
+import {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
+
+const Video = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return <div />;
 };
 
 export default Video;
+
+export const getStaticProps = async (ctx: GetStaticPropsContext) => {
+  const { data } = await client.query<VideoByIDQuery>({
+    query: VIDEO_BY_ID,
+    variables: {
+      id: ctx.params?.videoID,
+    },
+  });
+
+  return {
+    props: {
+      data: data.video,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query<{ videos: { id: string }[] }>({
+    query: gql`
+      query {
+        videos {
+          id
+        }
+      }
+    `,
+  });
+
+  const parsedIDs = data.videos.map((video) => ({
+    params: { videoID: video.id },
+  }));
+
+  return {
+    paths: parsedIDs,
+    fallback: "blocking",
+  };
+};
