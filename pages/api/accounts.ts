e563c337d77dcf18fb6e7a35697d2ac85b5f5ef4
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-06 20:35:44 -0300

    feat(api/): create accounts api to create a account for new users

--- pages/api/accounts.ts
+++ pages/api/accounts.ts
@@ -1,13 +1,61 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+  CREATE_ACCOUNT,
+  PUBLISH_INITIAL_ACCOUNT,
+} from "@core/graphql/mutations";
+import { client } from "@core/services";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
+type SuccessMessage = {
+  success: string;
 };
 
-export default function handler(
+type ErrorMessage = {
+  message: string;
+};
+
+type BodyParams = {
+  id: string;
+  username: string;
+  avatar: string | null;
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<SuccessMessage | ErrorMessage>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  if (req.method !== "POST")
+    return res.status(400).json({ message: "Method not allowed" });
+
+  const { id, username, avatar } = req.body as BodyParams;
+
+  if (!id || !username || !avatar) {
+    res.status(400).json({ message: "Missing body params" });
+  }
+
+  try {
+    //
+    await client.mutate({
+      mutation: CREATE_ACCOUNT,
+      variables: {
+        firestoreId: id,
+        profileUsername: username,
+        avatarUrl: avatar,
+      },
+    });
+
+    await client.mutate({
+      mutation: PUBLISH_INITIAL_ACCOUNT,
+      variables: {
+        firestoreId: id,
+      },
+    });
+
+    res.status(201).json({ success: "Account created" });
+    //
+  } catch (error) {
+    //
+    res.status(500).json({ message: "Internal error" });
+    //
+  }
 }
