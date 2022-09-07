5a91e630888602aff2d9d4597f8fca0b8becaf58
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-07 14:31:56 -0300

    feat(profilecontext): verify if a profile is selected
    if no profile selected, then redirect user to /profiles

--- core/contexts/ProfileContext/ProfileContext.tsx
+++ core/contexts/ProfileContext/ProfileContext.tsx
@@ -3,7 +3,8 @@ import {
   ProfileProviderProps,
   ProfileQuery,
 } from "@core/types";
-import { createContext, useState } from "react";
+import { useRouter } from "next/router";
+import { createContext, useEffect, useState } from "react";
 
 export const ProfileContext = createContext({} as ProfileContextValue);
 
@@ -11,6 +12,13 @@ export const ProfileProvider = ({ children }: ProfileProviderProps) => {
   const [selectedProfile, setSelectedProfile] = useState<
     ProfileQuery["profile"] | null
   >(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (selectedProfile === null) {
+      router.push("/profiles");
+    }
+  }, [selectedProfile, router]);
 
   function selectProfile(profile: ProfileQuery["profile"]) {
     setSelectedProfile(profile);
