ceca876361443134d5006876f175125488ef0f04
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-10 18:09:50 -0300

    feat(graphql mutations): create graphql mutations file

--- core/graphql/mutations.ts
+++ core/graphql/mutations.ts
@@ -30,3 +30,40 @@ export const PUBLISH_INITIAL_ACCOUNT = gql`
     }
   }
 `;
+
+export const ADD_PROFILE = gql`
+  mutation AddProfile(
+    $firestoreId: String!
+    $username: String!
+    $avatarUrl: String
+  ) {
+    updateAccount(
+      where: { firestoreId: $firestoreId }
+      data: {
+        profiles: { create: { username: $username, avatarUrl: $avatarUrl } }
+      }
+    ) {
+      profiles(where: { username: $username }) {
+        id
+        username
+        avatarUrl
+      }
+    }
+  }
+`;
+
+export const PUBLISH_ACCOUNT_AND_PROFILE = gql`
+  mutation PublishAccount($firestoreId: String!) {
+    publishAccount(to: PUBLISHED, where: { firestoreId: $firestoreId }) {
+      id
+    }
+
+    publishManyProfilesConnection(to: PUBLISHED) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`;
