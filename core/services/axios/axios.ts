71b60d7272b682d812877989b89624c9db3bd298
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-10 18:11:11 -0300

    refactor(axios service): change base url
    add /api on the end to access Next.js api routes directly

--- core/services/axios/axios.ts
+++ core/services/axios/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://disney-clone.vercel.app",
+      ? "http://localhost:3000/api"
+      : "https://disney-clone.vercel.app/api",
 });
