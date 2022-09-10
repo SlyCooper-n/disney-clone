8d8d9de1ce593d1f48fc7ceca6ea7a9761dc3205
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-09 23:30:10 -0300

    feat(homepage): add BannerSwiper component

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,4 +1,6 @@
+import { AuthGuard } from "@components/guards/AuthGuard/AuthGuard";
 import { PageContainer } from "@components/layouts";
+import { BannerSwiper } from "@components/widgets";
 import { HOMEPAGE } from "@core/graphql";
 import { client } from "@core/services";
 import { HomepageQuery } from "@core/types";
@@ -6,9 +8,11 @@ import type { InferGetStaticPropsType } from "next";
 
 const Home = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <PageContainer>
-      <h1>Hello World</h1>
-    </PageContainer>
+    <AuthGuard>
+      <PageContainer headTitle="Disney+ clone | Home">
+        <BannerSwiper />
+      </PageContainer>
+    </AuthGuard>
   );
 };
 
