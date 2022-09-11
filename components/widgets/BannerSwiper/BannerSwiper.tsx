92d9cd59cfd7d8653c7be67fe56111e20b6866f5
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-10 21:28:00 -0300

    refactor(bannerswiper): change slide element to anchor tag and link to movie details page on click

--- components/widgets/BannerSwiper/BannerSwiper.tsx
+++ components/widgets/BannerSwiper/BannerSwiper.tsx
@@ -1,5 +1,6 @@
 import { BannerSwiperProps } from "@core/types";
 import Image from "next/image";
+import Link from "next/link";
 import { Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -25,32 +26,34 @@ export const BannerSwiper = ({ slides }: BannerSwiperProps) => {
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
             {({ isActive }) => (
-              <div
-                className={`${
-                  !isActive && "brightness-50"
-                } relative h-full shadow-2xl transition-all duration-1000`}
-              >
-                <Image
-                  src={slide.imageUrl}
-                  alt={`${slide.title} banner`}
-                  layout="fill"
-                  className="object-cover object-top rounded-md"
-                />
-
-                <div
-                  className={`absolute top-1/2 left-12 -translate-y-1/2 w-1/3 aspect-video ${
-                    isActive
-                      ? "translate-x-0 opacity-100"
-                      : "translate-x-10 opacity-0"
-                  } transition-all duration-700 delay-200`}
+              <Link href={`/${slide.type}/${slide.slug}`}>
+                <a
+                  className={`${
+                    !isActive && "brightness-50"
+                  } relative block h-full hover:ring-2 hover:ring-white rounded-md shadow-2xl transition-all duration-1000`}
                 >
                   <Image
-                    src={slide.logo}
-                    alt={`${slide.title} logo`}
+                    src={slide.imageUrl}
+                    alt={`${slide.title} banner`}
                     layout="fill"
+                    className="object-cover object-top rounded-md"
                   />
-                </div>
-              </div>
+
+                  <div
+                    className={`absolute top-1/2 left-12 -translate-y-1/2 w-1/3 aspect-video ${
+                      isActive
+                        ? "translate-x-0 opacity-100"
+                        : "translate-x-10 opacity-0"
+                    } transition-all duration-700 delay-200`}
+                  >
+                    <Image
+                      src={slide.logo}
+                      alt={`${slide.title} logo`}
+                      layout="fill"
+                    />
+                  </div>
+                </a>
+              </Link>
             )}
           </SwiperSlide>
         ))}
