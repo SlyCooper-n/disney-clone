2ecc8f594f20109bbd08b0993973778ab749ade7
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-10 21:08:26 -0300

    feat(framer-motion-variants): add bannersVariants

---
+++ core/utils/framer-motion-variants.ts
@@ -0,0 +1,19 @@
+import { Variants } from "framer-motion";
+
+export const bannersVariants: Variants = {
+  initial: {
+    position: "absolute",
+    top: "50%",
+    left: "48",
+  },
+  hidden: {
+    opacity: 0,
+    x: 48,
+    transitionDelay: "3s",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transitionDelay: "3s",
+  },
+};
--- core/utils/index.ts
+++ core/utils/index.ts
@@ -1,1 +1,2 @@
+export * from "./framer-motion-variants";
 export * from "./menu-options";
