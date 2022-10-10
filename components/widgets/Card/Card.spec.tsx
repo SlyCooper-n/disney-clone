9ade6769d1a3d09405f3809ccecc9d0fe583d7ea
Gabriel <gabrielvitor.frasao@gmail.com>
2022-10-09 21:55:50 -0300

    test(card): add mocked data
    mock brand data to render Card component
    
    fix #11

--- components/widgets/Card/Card.spec.tsx
+++ components/widgets/Card/Card.spec.tsx
@@ -2,13 +2,25 @@ import { cleanup, render, screen } from "@testing-library/react";
 import { afterAll, describe, expect, it } from "vitest";
 import { Card } from "./Card";
 
+const mockedBrandData = {
+  id: "testId",
+  slug: "someBrand",
+  name: "Some Brand",
+  logo: "https://picsum.photos/200",
+  backgroundGif: "https://picsum.photos/200",
+};
+
 describe("Card component", () => {
   afterAll(() => {
     cleanup();
   });
 
   it("should render 'brand' variant", () => {
-    render(<Card variant="brand" />);
+    const { debug } = render(
+      <Card variant="brand" brandData={mockedBrandData} />
+    );
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    debug();
 
     expect(screen.getByTestId("brand-card-link")).toContainElement(
       screen.getByTestId("brand-card-video")
